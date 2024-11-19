12c6025a3e438389b37bcfad4bc52ec4b4de10af
Jaden Rodriguez <jjrodriguez-@cps.edu>
2024-11-19 12:11:35 -0600

    11-19-24

--- ControlFlowLabPart3/main.swift
+++ ControlFlowLabPart3/main.swift
@@ -167,7 +167,17 @@ print("***PROBLEM 3.4 : FINISHING THE FITNESS APP")
 
 var currentHr = 90
 var targetHr = 100
+let isBelowTarget = currentHr < targetHr
+let isInTarget = currentHr == targetHr
+let isAboveTarget = currentHr > targetHr
 
+if isInTarget {
+    print("You're right on track!")
+} else if isBelowTarget {
+    print("You're doing great! but try to push it a bit!")
+} else {
+    print("You're on fire! Slow it down just a bit")
+}
 
 
 //Once you complete the coding, test and run the code with various values for currentHR to see if all the coding works. If all the coding works, check your work and see if it shows in the console. Then you are done! Push it up to GitHub.
